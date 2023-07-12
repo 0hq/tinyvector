36aa06b1e6e80b3f438ba4ea666176bec47fb634
Anant <anantsnh@umich.edu>
2023-07-11 21:43:06 -0400

    Update version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tinyvector',
-    version='0.1.0',
+    version='0.1.1',
     author='Will DePue',
     author_email='will@depue.net',
     license='MIT',
