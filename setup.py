3ed9a82546a4fcae43dfcedbf35362606e5326c6
Will DePue <will@depue.net>
2023-07-11 17:41:23 -0700

    modify setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     author_email='will@depue.net',
     license='MIT',
     description='the tiny, least-dumb, speedy vector embedding database.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/0hq/tinyvector',
     packages=find_packages(include=['core', 'core.*']),
     install_requires=[
         'numpy',
